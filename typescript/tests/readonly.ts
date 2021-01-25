f832e416acbe94c3c7cd13603fc75468bedd0334
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-01-25 10:49:47 +0800

    readonly properties in TypeScript

---
+++ typescript/tests/readonly.ts
@@ -0,0 +1,29 @@
+type DeepReadonlyObject<T> = { readonly [K in keyof T]: DeepReadOnly<T[K]> }
+
+type DeepReadOnly<T> = T extends (infer E)[]
+  ? ReadonlyArray<DeepReadonlyObject<E>>
+  : T extends object
+  ? DeepReadonlyObject<T>
+  : T
+
+interface IShape {
+  type: string
+}
+
+interface ICompositeShape extends IShape {
+  childItems: IShape[]
+}
+
+type ReadonlyShape = DeepReadOnly<ICompositeShape>
+
+const obj1 = {
+  type: "shape1",
+  childItems: [{ type: "shape2" }, { type: "shape3" }]
+}
+
+obj1.childItems[0].type = "jjj"
+
+const readOnlyObj1: ReadonlyShape = obj1
+
+// Error
+readOnlyObj1.childItems[0].type = ""
